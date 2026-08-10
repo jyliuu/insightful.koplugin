722f87a007a356193824a5cb5f7dcf7a19dcd49e
Jinyang Liu <jl@math.ku.dk>
2026-08-10 23:40:32 +0200

    chore(release): v0.2.2

--- _meta.lua
+++ _meta.lua
@@ -4,5 +4,5 @@ return {
     name = "insightful",
     fullname = _("Insightful"),
     description = _("An AI reading companion with saved chats and bounded access to the open book"),
-    version = "0.2.1",
+    version = "0.2.2",
 }
